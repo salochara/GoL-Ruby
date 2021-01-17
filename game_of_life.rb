3a5323c7598e10d9b0517bfea40e4acc5ccfe1da
Salo Charabati <salochara@gmail.com>
2021-01-17 16:38:00 -0600

    initial world awith intial states of cells methods are done

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,7 +1,37 @@
 class Cell
+  attr_accessor :alive
   def initialize(x,y)
     @x = x
     @y = y
+    @alive = false
   end
 end
 
+
+class World
+  def initialize(rows = 3, cols = 3)
+    @rows = rows
+    @cols = cols
+    @cells = []
+
+    @world = Array.new(@rows) do |row|
+      Array.new(@cols) do |col|
+        cell = Cell.new(row,col)
+        @cells << cell
+      end
+    end
+  end
+
+  def set_initial_state
+    @world = @cells.each do |cell|
+      cell.alive = [false, true].sample
+    end
+  end
+
+end
+
+world = World.new
+
+p '-----'
+world.set_initial_state
+p world
