d4221fed0b72f6102c4ca568ebd99cb272347337
Salo Charabati <salochara@gmail.com>
2021-01-19 17:13:00 -0600

    added comments to the file and fixed styling issues

--- world.rb
+++ world.rb
@@ -1,19 +1,23 @@
-require_relative 'cell'
+# frozen_string_literal: true
 
+require_relative 'cell'
+# The World class is responsible for creating a world with a Matrix.
+# The Matrix data structure is defined in the 'matrix.rb' file included in this project
 class World
-  attr_accessor :matrix
+  attr_reader :matrix
+
   def initialize
     @matrix = Matrix[
-      [Cell.new(0,0 ),Cell.new(0,1),Cell.new(0,2),Cell.new(0,3)],
-      [Cell.new(1,0),Cell.new(1,1),Cell.new(1,2),Cell.new(1,3)],
-      [Cell.new(2,0),Cell.new(2,1),Cell.new(2,2),Cell.new(2,3)],
-      [Cell.new(3,0),Cell.new(3,1),Cell.new(3,2),Cell.new(3,3)]
+      [Cell.new(0, 0), Cell.new(0, 1), Cell.new(0, 2), Cell.new(0, 3)],
+      [Cell.new(1, 0), Cell.new(1, 1), Cell.new(1, 2), Cell.new(1, 3)],
+      [Cell.new(2, 0), Cell.new(2, 1), Cell.new(2, 2), Cell.new(2, 3)],
+      [Cell.new(3, 0), Cell.new(3, 1), Cell.new(3, 2), Cell.new(3, 3)]
     ]
   end
 
   def randomly_populate
-    self.matrix.each do |cell|
-      cell.alive = [true,false].sample
+    matrix.each do |cell|
+      cell.alive = [true, false].sample
     end
   end
 end
