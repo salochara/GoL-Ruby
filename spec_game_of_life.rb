3e3f8ae0cf9b02925c243f6af4979fc3cea10c02
Salo Charabati <salochara@gmail.com>
2021-01-20 10:34:17 -0600

    added tests for Cell class

--- spec_game_of_life.rb
+++ spec_game_of_life.rb
@@ -1,6 +1,27 @@
-require_relative 'game_of_life.rb'
+# frozen_string_literal: true
+require_relative 'game_of_life'
 require 'rspec'
 
 describe 'Game of Life' do
+
+  context 'cell' do
+    subject { Cell.new(0, 0) }
+
+    it 'should create a new cell object' do
+      expect(subject).to be_a Cell
+    end
+    it 'should initialize as a dead cell' do
+      expect(subject.alive).to be false
+    end
+    it 'should initialize with arguments passed in cell.new() method ' do
+      expect(subject.x_position).to be 0
+      expect(subject.y_position).to be 0
+    end
+    it 'should correctly respond to alive? method' do
+      expect(subject.alive?).to be false
+    end
+  end
+
+
 
 end
