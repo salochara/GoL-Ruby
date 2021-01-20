acfb2d605cab56854ca2f63c44d788fd9f57dd5b
Salo Charabati <salochara@gmail.com>
2021-01-20 10:33:47 -0600

    fixed styling rubocop issues

--- cell.rb
+++ cell.rb
@@ -7,7 +7,7 @@ class Cell
   attr_accessor :alive
   attr_reader :x_position, :y_position
 
-  def initialize(x_position,y_position)
+  def initialize(x_position, y_position)
     @x_position = x_position
     @y_position = y_position
     @alive = false
